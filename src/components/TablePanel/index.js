96600200ec5ea5fbd8b66d2bf2b489bfe74f5ce2
Christian Gastrell <cgastrell@gmail.com>
2018-07-26 03:07:41 -0300

    refine

--- src/components/TablePanel/index.js
+++ src/components/TablePanel/index.js
@@ -5,19 +5,20 @@ import classnames from 'classnames'
 import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
 import CardContent from '@material-ui/core/CardContent'
-// import CardActions from '@material-ui/core/CardActions'
-// import FavoriteIcon from '@material-ui/icons/Favorite'
+import CardActions from '@material-ui/core/CardActions'
+import AttachmentIcon from '@material-ui/icons/Attachment'
 // import ShareIcon from '@material-ui/icons/Share'
 // import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
 import Avatar from '@material-ui/core/Avatar'
 import IconButton from '@material-ui/core/IconButton'
+import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
 // import Menu from '@material-ui/core/Menu'
 // import MenuItem from '@material-ui/core/MenuItem'
 // import Typography from '@material-ui/core/Typography'
 import avatarColor from '@material-ui/core/colors/green'
-import SaveIcon from '@material-ui/icons/Save'
+import SaveIcon from '@material-ui/icons/CloudDownload'
 import AddressTable from './AddressTable'
 import XLSX from 'xlsx'
 import Nprogress from 'nprogress'
@@ -246,6 +247,9 @@ class TablePanel extends React.PureComponent {
   handleReset (event) {
     this.setState(defaultState)
   }
+  handleSampleDownload (event) {
+
+  }
   // handleExpandClick = () => {
   //   this.setState(state => ({ expanded: !state.expanded }))
   // }
@@ -347,7 +351,22 @@ class TablePanel extends React.PureComponent {
                 <Tooltip
                   disableFocusListener={true}
                   disableTouchListener={true}
-                  title="Save xlsx">
+                  title="Geocode!">
+                  <IconButton
+                    color="secondary"
+                    disabled={!hasFile}
+                    onClick={this.handleMegaGeocoder}>
+                    {
+                      this.props.geocoding
+                        ? <CircularProgress color="secondary" />
+                        : <GeocoderIcon />
+                    }
+                  </IconButton>
+                </Tooltip>
+                <Tooltip
+                  disableFocusListener={true}
+                  disableTouchListener={true}
+                  title="Save .xlsx">
                   <IconButton
                     color={hasFile ? 'primary' : 'default'}
                     disabled={!hasFile || this.props.geocoding}
@@ -364,21 +383,6 @@ class TablePanel extends React.PureComponent {
                     disabled={!hasFile || this.props.geocoding}
                     onClick={this.handleReset}>
                     <DeleteIcon />
-                  </IconButton>
-                </Tooltip>
-                <Tooltip
-                  disableFocusListener={true}
-                  disableTouchListener={true}
-                  title="Geocode!">
-                  <IconButton
-                    color="secondary"
-                    disabled={!hasFile}
-                    onClick={this.handleMegaGeocoder}>
-                    {
-                      this.props.geocoding
-                        ? <CircularProgress color="secondary" />
-                        : <GeocoderIcon />
-                    }
                   </IconButton>
                 </Tooltip>
               </React.Fragment>
@@ -399,14 +403,20 @@ class TablePanel extends React.PureComponent {
           <CardContent className={classes.cardContent}>
             <AddressTable geocode={this.geocodeRow} data={this.state.data} cols={this.state.cols.filter(geocoderColumns)} />
           </CardContent>
-          {/* <CardActions className={classes.actions} disableActionSpacing>
-            <IconButton aria-label="Add to favorites">
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="Share">
+          <CardActions className={classes.actions} disableActionSpacing>
+            <Button
+              href="test.xlsx"
+              component="a"
+              download="test.xlsx"
+              // onClick={this.handleSampleDownload}
+              aria-label="Download sample file">
+              Sample file (for testing)
+              <AttachmentIcon />
+            </Button>
+            {/* <IconButton aria-label="Share">
               <ShareIcon />
-            </IconButton>
-            <IconButton
+            </IconButton> */}
+            {/* <IconButton
               className={classnames(classes.expand, {
                 [classes.expandOpen]: this.state.expanded,
               })}
@@ -415,8 +425,8 @@ class TablePanel extends React.PureComponent {
               aria-label="Show more"
             >
               <ExpandMoreIcon />
-            </IconButton>
-          </CardActions> */}
+            </IconButton> */}
+          </CardActions>
 
         </Card>
       </div>
