598694d1518b36529500b302296d03d428a0c83c
Christian Gastrell <cgastrell@gmail.com>
2018-08-30 02:01:12 -0300

    key reducer to actually do something with the key

--- src/reducers/key.js
+++ src/reducers/key.js
@@ -1,5 +1,8 @@
+import { KEY_SAVE, KEY_SET } from '../actions/misc'
 export default (state = '', action) => {
   switch (action.type) {
+    case KEY_SET: return action.key
+    case KEY_SAVE: return action.key
     default: return state
   }
 }
