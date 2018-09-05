5da0d41161321289f855a0bb35256d43e531f382
Christian Gastrell <cgastrell@gmail.com>
2018-09-05 12:12:14 -0300

    add handler to edit lat/lngs

--- src/components/TablePanel/AddressRow.js
+++ src/components/TablePanel/AddressRow.js
@@ -9,7 +9,7 @@ import GeocodeIcon from '@material-ui/icons/Place'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
 import { connect } from 'react-redux'
-import { geocode, editAddress } from '../../actions/file'
+import { receiveGeocode, geocode, editAddress } from '../../actions/file'
 const styles = theme => {
   // console.log(theme)
   return {
@@ -21,14 +21,67 @@ const styles = theme => {
 class AddressRow extends React.PureComponent {
   constructor (props) {
     super(props)
-    this.handleRowEdit = this.handleRowEdit.bind(this)
+    this.handleAddressEdit = this.handleAddressEdit.bind(this)
     this.handleGeocode = this.handleGeocode.bind(this)
+    this.handleLatLonEdit = this.handleLatLonEdit.bind(this)
   }
-  handleRowEdit (event) {
+  handleLatLonEdit (event) {
+    const frame = 0.0014
+    // make a result-like object
+    const lat = event.target.name === 'lat'
+      ? Number(event.target.value)
+      : Number(this.props.row['lat'])
+    const lng = event.target.name === 'lon'
+      ? Number(event.target.value)
+      : Number(this.props.row['lon'])
+    const geom = {
+      location: {
+        lat: lat,
+        lng: lng
+      },
+      viewport: {
+        northeast: {
+          lat: lat + frame, // lat + 0.0014
+          lng: lng + frame
+        },
+        southwest: {
+          lat: lat - frame,
+          lng: lng - frame
+        }
+      }
+    }
+    this.props.receiveGeocode(this.props.row, geom, this.props.rowIndex)
+  }
+  handleAddressEdit (event) {
     this.props.editAddress(this.props.rowIndex, event.target.value)
   }
   handleGeocode (event) {
     this.props.geocode(this.props.row, this.props.apiKey, this.props.rowIndex)
+  }
+  renderCell (col) {
+    switch (col) {
+      case 'address':
+        return (
+          <textarea
+            style={{width: '100%', border: 'none', fontSize: '13px', boxSizing: 'border-box'}}
+            onChange={this.handleAddressEdit}
+            defaultValue={this.props.row[col]}>
+          </textarea>
+        )
+      case 'lat':
+      case 'lon':
+        return (
+          <input
+            name={col}
+            onChange={this.handleLatLonEdit}
+            style={{padding: '8px', fontSize: '13px', border: 0, width: '100%', textAlign: 'right'}}
+            step="0.00001"
+            type="number"
+            defaultValue={this.props.row[col]}
+          />
+        )
+      default: return this.props.row[col]
+    }
   }
   render () {
     return (
@@ -37,21 +90,11 @@ class AddressRow extends React.PureComponent {
         {
           this.props.cols.map((c) => (
             <TableCell
-              // onChange={this.handleRowEdit}
-              // contentEditable={(c.name === 'address')}
               padding="dense"
               key={c.key}
               numeric={(c.name === 'lat' || c.name === 'lon')}
               >
-                {
-                  c.name === 'address'
-                    ? <textarea
-                        style={{width: '100%', border: 'none', fontSize: '13px', boxSizing: 'border-box'}}
-                        onChange={this.handleRowEdit}
-                        defaultValue={this.props.row[c.name]}>
-                      </textarea>
-                    : this.props.row[c.name]
-                }
+                {this.renderCell(c.name)}
             </TableCell>
           ))
         }
@@ -82,7 +125,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     geocode: (row, key, rowIndex) => dispatch(geocode(row, key, rowIndex)),
-    editAddress: (row, address) => dispatch(editAddress(row, address))
+    editAddress: (row, address) => dispatch(editAddress(row, address)),
+    receiveGeocode: (row, geom, rowIndex) => dispatch(receiveGeocode(row, geom, rowIndex))
   }
 }
 export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(AddressRow))
