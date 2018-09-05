141622f1b0b0742a27b7aa629e5750529448145c
Christian Gastrell <cgastrell@gmail.com>
2018-09-05 12:11:29 -0300

    no row: marker drag, don't mess the map; no geom: nothing to do with the map

--- src/reducers/map.js
+++ src/reducers/map.js
@@ -4,7 +4,7 @@ import { GEOCODE_RECEIVE } from '../actions/file'
 export default (state = defaultState.mapParams, action) => {
   switch (action.type) {
     case GEOCODE_RECEIVE:
-      if (!action.row) return state
+      if (!action.row || !action.geom) return state
       return {
         ...state,
         center: [action.geom.location.lat, action.geom.location.lng],
